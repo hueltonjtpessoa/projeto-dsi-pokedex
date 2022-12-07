fe48de344d1b672f4795741ff410e73ada8a2403
André Veiga <100880823+andreveigah@users.noreply.github.com>
2022-12-07 16:40:42 -0300

    Update ResultadoApi.kt

--- app/src/main/java/br/com/projeto_dsi_pokedex/api/model/ResultadoApi.kt
+++ app/src/main/java/br/com/projeto_dsi_pokedex/api/model/ResultadoApi.kt
@@ -2,7 +2,7 @@ package br.com.projeto_dsi_pokedex.api.model
 
 import br.com.projeto_dsi_pokedex.dados.Pokemon
 import br.com.projeto_dsi_pokedex.dados.TipoPokemon
-
+// classes regulares voltadas para o armazenamento de estado
 data class ResultadoApi(
     val count: Int,
     //next, previous são opcionais, podem acertar. Por isso o uso da "?" para mostrar que nem sempre estão aqui.
