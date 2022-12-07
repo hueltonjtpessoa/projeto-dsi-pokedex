87f60a02cd138b7a45baad61a868ecfafda0ecf5
Heder Barreto <matheusmr640@gmail.com>
2022-12-07 11:15:23 -0300

    add Repositorio

--- app/src/main/java/br/com/projeto_dsi_pokedex/api/Repositorio.kt
+++ app/src/main/java/br/com/projeto_dsi_pokedex/api/Repositorio.kt
@@ -1,4 +1,31 @@
 package br.com.projeto_dsi_pokedex.api
 
+import br.com.projeto_dsi_pokedex.api.model.PokemonApiResultado
+import br.com.projeto_dsi_pokedex.api.model.ResultadoApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object Repositorio {
+    private val service:Service
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        service = retrofit.create(Service::class.java)
+    }
+
+    fun listPokemons(limit: Int = 251): ResultadoApi? {
+        val call = service.listaPokemons(limit)
+
+        return call.execute().body()
+
+    }
+
+    fun getPokemon(number:Int): PokemonApiResultado? {
+        val call = service.getPokemon(number)
+
+        return call.execute().body()
+
+    }
 }
