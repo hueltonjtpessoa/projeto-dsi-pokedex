db9b50001f31eee759f46140d09194ef9093ce05
André Veiga <100880823+andreveigah@users.noreply.github.com>
2022-12-07 13:05:51 -0300

    repor e servi

--- app/src/main/java/br/com/projeto_dsi_pokedex/api/Repositorio.kt
+++ app/src/main/java/br/com/projeto_dsi_pokedex/api/Repositorio.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object Repositorio {
     private val service:Service
     init {
-        val retrofit = Retrofit.Builder()
+        val retrofit = Retrofit.Builder() //biblioteca Retrofit para requisições HTTP
             .baseUrl("https://pokeapi.co/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
